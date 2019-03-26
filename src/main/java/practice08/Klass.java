77637c6c3b6dafe465cd40d876d808692298525c
Qianru <2583543607@qq.com>
2019-03-26 16:38:13 +0800

    pratice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,40 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Klass klass = (Klass) o;
+
+        return number == klass.number;
+    }
+
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass().equals(this)) {
+            this.leader = student;
+        } else {
+            System.out.println("It is not one of us.");
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
 }
