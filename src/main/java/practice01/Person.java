c87f2ce0868a97ba06ab258236f476c0d9f5861d
Qianru <2583543607@qq.com>
2019-03-26 16:19:28 +0800

    practice01

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,30 @@
 package practice01;
 
 public class Person {
+	private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
 }
+/*
+写一个Person类，要有name，age属性，要有一个introduce方法，
+introduce方法返回一个字符串形如：
+
+>My name is Tom. I am 21 years old.
+
+*/
