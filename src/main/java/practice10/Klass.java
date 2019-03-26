526edb275f0e3bad5e24744897634fd8472bd330
Qianru <2583543607@qq.com>
2019-03-26 16:41:13 +0800

    practice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,43 @@
 package practice10;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Klass klass = (Klass) o;
+
+        return number == klass.number;
+    }
+	
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if (!student.getKlass().equals(this)) {
+            System.out.print("It is not one of us.\n");
+        } else {
+            this.leader = student;
+        }
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,61 @@
 package practice10;
 
-public class Teacher {
+import java.util.Collection;
+import java.util.LinkedList;
+
+import static java.util.stream.Collectors.joining;
+
+public class Teacher extends Person {
+    private LinkedList<Klass> classes = new LinkedList<Klass>();
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public String introduce() {
+        if (this.classes.isEmpty()) {
+            return teach("No Class");
+        }
+
+        String classIds = this.classes.stream().map(k -> String.valueOf(k.getNumber())).collect(joining(", "));
+        return teach("Class " + classIds);
+    }
+
+    private String teach(String name) {
+        return basicIntroduce() + " I teach " + name + ".";
+    }
+
+    private String doNotTeach(String name) {
+        return basicIntroduce() + " I don't teach " + name + ".";
+    }
+
+    public String introduceWith(Student student) {
+        if (isTeaching(student)) {
+            return teach(student.getName());
+        }
+        return doNotTeach(student.getName());
+    }
+
+    private String basicIntroduce() {
+        return super.introduce() + " I am a Teacher.";
+    }
+
+    public Collection<Klass> getClasses() {
+        return classes;
+    }
+
+    public boolean isTeaching(Student student) {
+		Klass k = student.getKlass();
+		for(Klass elem:this.classes)
+		{
+			if(k.equals(elem))
+				return true;
+		}
+        return false;
+    }
 }
