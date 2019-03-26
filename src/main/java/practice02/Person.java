5d17aaee9617775f38bf88de0f1cadc13cfe5c6a
Qianru <2583543607@qq.com>
2019-03-26 16:30:04 +0800

    practice02

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,34 @@
 package practice02;
 
 public class Person {
+	private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
 }
+/*
+写一个Person类，要有name，age属性，要有一个introduce方法，
+introduce方法返回一个字符串形如：
+
+>My name is Tom. I am 21 years old.
+
+再写一个Student类继承Person类，除了name，age属性，还有要有klass属性。也有一个introduce方法，
+introduce方法返回一个字符串形如：
+
+>I am a Student. I am at Class 2.
+*/
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,24 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+	private final int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class " + this.klass + ".";
+    }
 }
+/*
+再写一个Student类继承Person类，除了name，age属性，还有要有klass属性。也有一个introduce方法，
+introduce方法返回一个字符串形如：
+
+>I am a Student. I am at Class 2.
+*/
