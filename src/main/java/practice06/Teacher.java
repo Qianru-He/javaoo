9afb0526b5999043e4c584e4e5fb221065d011d4
Qianru <2583543607@qq.com>
2019-03-26 16:35:42 +0800

    practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private final int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+		this.klass = 0;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+	public int getKlass() {
+        return klass;
+    }
+    @Override
+    public String introduce() {
+        if (this.klass == 0) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return super.introduce() + " I am a Teacher. I teach Class "+this.klass+ ".";
+    }
+
 }
